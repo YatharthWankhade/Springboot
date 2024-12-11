57cc229eadeb5ba93ebe3d9de6cb6225f711422f
YatharthWankhade <yatharthwankhade2001@gmail.com>
2024-12-12 00:21:58 +0530

    Day6

--- Project/blogapp/src/main/java/com/wayne/blogapp/controller/BlogEntryControllerV2.java
+++ Project/blogapp/src/main/java/com/wayne/blogapp/controller/BlogEntryControllerV2.java
@@ -2,9 +2,12 @@ package com.wayne.blogapp.controller;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,34 +28,48 @@ public class BlogEntryControllerV2 {
     private BlogEntryService blogEntryService;
 
     @GetMapping
-    public List<BlogEntry> getAll(){
-        return blogEntryService.getAll();
+    public ResponseEntity<?> getAll(){
+        List<BlogEntry> all = blogEntryService.getAll();
+        if(all != null && !all.isEmpty()){
+            return new ResponseEntity<>(all, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
     @PostMapping
-    public BlogEntry createEntry(@RequestBody BlogEntry myEntry){
-        myEntry.setDate(LocalDateTime.now());
-        blogEntryService.saveEntry(myEntry);
-        return myEntry;
+    public ResponseEntity<BlogEntry> createEntry(@RequestBody BlogEntry myEntry){
+        try {
+            myEntry.setDate(LocalDateTime.now());
+            blogEntryService.saveEntry(myEntry);
+            return new ResponseEntity<>(myEntry, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(myEntry, HttpStatus.BAD_REQUEST);
+        }
+        
     }
     @GetMapping("id/{id}")
-    public BlogEntry getBlogEntryById(@PathVariable ObjectId id){
-        return blogEntryService.findById(id).orElse(null);
+    public ResponseEntity<BlogEntry> getBlogEntryById(@PathVariable ObjectId id){
+        Optional<BlogEntry> BlogEntry = blogEntryService.findById(id);
+        if(BlogEntry.isPresent()){
+            return new ResponseEntity<>(BlogEntry.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
     @DeleteMapping("id/{id}")
-    public boolean deleteBlogEntryById(@PathVariable ObjectId id){
+    public ResponseEntity<BlogEntry>  deleteBlogEntryById(@PathVariable ObjectId id){
         blogEntryService.deleteById(id);
-        return true;
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
     @SuppressWarnings("unlikely-arg-type")
     @PutMapping("/id/{id}")
-    public BlogEntry updateBlogEntryById(@PathVariable ObjectId id, @RequestBody BlogEntry newEntry){
+    public ResponseEntity<?> updateBlogEntryById(@PathVariable ObjectId id, @RequestBody BlogEntry newEntry){
         BlogEntry old = blogEntryService.findById(id).orElse(null);
         if(old!= null){
             old.setTitle(newEntry.getTitle()!= null && !newEntry.getTitle().equals("") ? newEntry.getTitle() : old.getTitle());
             old.setContent(newEntry.getContent()!= null && !newEntry.equals("") ? newEntry.getContent() : old.getContent());
+            blogEntryService.saveEntry(old);
+            return new ResponseEntity<>(HttpStatus.OK);
         }
-        blogEntryService.saveEntry(old);
-        return old;
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
 
